79974699ff76a31e6156e717d97435c708b69824
Anita <52110800+PoudyalAnna@users.noreply.github.com>
2019-08-06 11:30:12 -0500

    change my code.

---
+++ HomeWorkExe/HomeWorkExe/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkExe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNo;
+            int secondNo;
+            int sumNo;
+            int divNo;
+            int MultiNo;
+            int quotNo;
+
+            Console.WriteLine("Please Enter First No.");
+            firstNo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter Second No.");
+            firstNo = Convert.ToInt32(Console.ReadLine());
+
+            //calculation
+
+
+        }
+    }
+}
---
+++ HomeWorkch_1/HomeWorkch_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkch_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("THis is change i made");
+            Console.WriteLine("I am learning.");
+
+        }
+    }
+}
